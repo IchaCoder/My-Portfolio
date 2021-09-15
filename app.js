3e5b7d3e941a4fbc0a6e18f9603d4356a5f1d74e
IchaCoder <76918241+IchaCoder@users.noreply.github.com>
2021-09-15 22:38:48 +0100

    services
    started the services section

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // Selecting elements and buttons
+const nameDiv = document.querySelector('.name');
 
 const menuBtn = document.querySelector('.fa-bars');
 const menuDiv = document.querySelector('.menu-btn');
 const header = document.querySelector('.main');
 const closeBtn = document.querySelector('div > .fa-times');
+const topBtn = document.querySelector('.to-top');
+
 
 menuBtn.addEventListener('click', function(){
     // Selecting the class list
     const btnClassList = menuDiv.classList;
     const headerClassList = header.classList;
+    const scrollPoint = window.pageYOffset;
     
     // Checking if the class list contains 'hide-main' class
         headerClassList.remove('hide-main')
@@ -16,6 +20,8 @@ menuBtn.addEventListener('click', function(){
 
     // Adding the close button
     btnClassList.add('show-btn');
+    //nameDiv.style.display = 'none'
+
 })
 
 closeBtn.addEventListener('click', function(){
@@ -29,17 +35,25 @@ closeBtn.addEventListener('click', function(){
 
 
 // selecting the name
-const nameDiv = document.querySelector('.name');
 
 window.addEventListener('scroll', function(){
     // Getting the scroll point number
     const scrollPoint = window.pageYOffset;
     const nameClassList = nameDiv.classList;
-    console.log(scrollPoint);
+    const toTop = topBtn.classList;
+
+    //console.log(scrollPoint);
     if(scrollPoint > 405){
         nameClassList.add('useFocus')
     } else {
         nameClassList.remove('useFocus')
 
+    };
+
+    if(scrollPoint > 600){
+        toTop.remove('hide-btn')
+    } else {
+        toTop.add('hide-btn')
     }
+    
 })
